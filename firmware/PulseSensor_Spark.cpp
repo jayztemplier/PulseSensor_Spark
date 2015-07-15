94baaa6218a8175d1b001ba5aac2ca2c9bcdfb6e
Jeremy Templier <templier.jeremy@gmail.com>
2015-07-14 20:38:59 -0700

    add support for second sensor on A3

--- firmware/PulseSensor_Spark.cpp
+++ firmware/PulseSensor_Spark.cpp
@@ -5,68 +5,107 @@ void interruptSetup(void){
   // Allocate a timer to throw an interrupt every 2mS.
   pulseTimer.begin(pulseISR, 2000, uSec);  // blinkLED to run every 2ms (2000 * 1us period)
 
-} 
+}
 
 
-// THIS IS THE TIMER 2 INTERRUPT SERVICE ROUTINE. 
+// THIS IS THE TIMER 2 INTERRUPT SERVICE ROUTINE.
 // Timer 2 makes sure that we take a reading every 2 miliseconds
 //ISR(TIMER2_COMPA_vect){                         // triggered when Timer2 counts to 124
 void pulseISR(void) {
   noInterrupts();
-  Signal = analogRead(pulsePin);              // read the Pulse Sensor 
+  Signal = analogRead(pulsePin);              // read the Pulse Sensor
+  Signal2 = analogRead(pulsePin2);              // read the Pulse Sensor
+
   sampleCounter += 2;                         // keep track of the time in mS with this variable
   int N = sampleCounter - lastBeatTime;       // monitor the time since the last beat to avoid noise
 
     //  find the peak and trough of the pulse wave
   if(Signal < thresh && N > (IBI/5)*3){       // avoid dichrotic noise by waiting 3/5 of last IBI
     if (Signal < T){                        // T is the trough
-      T = Signal;                         // keep track of lowest point in pulse wave 
+      T = Signal;                         // keep track of lowest point in pulse wave
+    }
+  }
+  if(Signal2 < thresh2 && N > (IBI/5)*3){       // avoid dichrotic noise by waiting 3/5 of last IBI
+    if (Signal2 < T2){                        // T is the trough
+      T2 = Signal2;                         // keep track of lowest point in pulse wave
     }
   }
 
   if(Signal > thresh && Signal > P){          // thresh condition helps avoid noise
     P = Signal;                             // P is the peak
   }                                        // keep track of highest point in pulse wave
+  if(Signal2 > thresh2 && Signal2 > P2){          // thresh condition helps avoid noise
+    P2 = Signal2;                             // P is the peak
+  }
 
   //  NOW IT'S TIME TO LOOK FOR THE HEART BEAT
   // signal surges up in value every time there is a pulse
   if (N > 250){                                   // avoid high frequency noise
-    if ( (Signal > thresh) && (Pulse == false) && (N > (IBI/5)*3) ){        
+    if ( ((Signal > thresh) || (Signal2 > thresh2)) && (Pulse == false) && (N > (IBI/5)*3) ){
       Pulse = true;                               // set the Pulse flag when we think there is a pulse
       digitalWrite(blinkPin,HIGH);                // turn on pin 13 LED
       IBI = sampleCounter - lastBeatTime;         // measure time between beats in mS
       lastBeatTime = sampleCounter;               // keep track of time for next pulse
 
-      if(secondBeat){                        // if this is the second beat, if secondBeat == TRUE
-        secondBeat = false;                  // clear secondBeat flag
-        for(int i=0; i<=9; i++){             // seed the running total to get a realisitic BPM at startup
-          rate[i] = IBI;                      
+      if ((Signal > thresh)) {
+        if(secondBeat){                        // if this is the second beat, if secondBeat == TRUE
+          secondBeat = false;                  // clear secondBeat flag
+          for(int i=0; i<=9; i++){             // seed the running total to get a realisitic BPM at startup
+            rate[i] = IBI;
+          }
         }
+        if(firstBeat){                         // if it's the first time we found a beat, if firstBeat == TRUE
+          firstBeat = false;                   // clear firstBeat flag
+          secondBeat = true;                   // set the second beat flag
+  		interrupts();
+          return;                              // IBI value is unreliable so discard it
+        }
+        // keep a running total of the last 10 IBI values
+        uint16_t runningTotal = 0;              // clear the runningTotal variable
+
+        for(int i=0; i<=8; i++){                // shift data in the rate array
+          rate[i] = rate[i+1];                  // and drop the oldest IBI value
+          runningTotal += rate[i];              // add up the 9 oldest IBI values
+        }
+
+        rate[9] = IBI;                          // add the latest IBI to the rate array
+        runningTotal += rate[9];                // add the latest IBI to runningTotal
+        runningTotal /= 10;                     // average the last 10 IBI values
+        BPM = 60000/runningTotal;               // how many beats can fit into a minute? that's BPM!
+        QS = true;                              // set Quantified Self flag
+        // QS FLAG IS NOT CLEARED INSIDE THIS ISR
+      }
+      if ((Signal2 > thresh2)) {
+        if(secondBeat2){                        // if this is the second beat, if secondBeat == TRUE
+          secondBeat2 = false;                  // clear secondBeat flag
+          for(int i=0; i<=9; i++){             // seed the running total to get a realisitic BPM at startup
+            rate2[i] = IBI;
+          }
+        }
+        if(firstBeat2){                         // if it's the first time we found a beat, if firstBeat == TRUE
+          firstBeat2 = false;                   // clear firstBeat flag
+          secondBeat2 = true;                   // set the second beat flag
+      interrupts();
+          return;                              // IBI value is unreliable so discard it
+        }
+        // keep a running total of the last 10 IBI values
+        uint16_t runningTotal = 0;              // clear the runningTotal variable
+
+        for(int i=0; i<=8; i++){                // shift data in the rate array
+          rate2[i] = rate2[i+1];                  // and drop the oldest IBI value
+          runningTotal += rate2[i];              // add up the 9 oldest IBI values
+        }
+
+        rate2[9] = IBI;                          // add the latest IBI to the rate array
+        runningTotal += rate2[9];                // add the latest IBI to runningTotal
+        runningTotal /= 10;                     // average the last 10 IBI values
+        BPM2 = 60000/runningTotal;               // how many beats can fit into a minute? that's BPM!
+        QS = true;                              // set Quantified Self flag
+        // QS FLAG IS NOT CLEARED INSIDE THIS ISR
+
       }
 
-      if(firstBeat){                         // if it's the first time we found a beat, if firstBeat == TRUE
-        firstBeat = false;                   // clear firstBeat flag
-        secondBeat = true;                   // set the second beat flag
-		interrupts();
-        return;                              // IBI value is unreliable so discard it
-      }   
-
-
-      // keep a running total of the last 10 IBI values
-      uint16_t runningTotal = 0;              // clear the runningTotal variable    
-
-      for(int i=0; i<=8; i++){                // shift data in the rate array
-        rate[i] = rate[i+1];                  // and drop the oldest IBI value 
-        runningTotal += rate[i];              // add up the 9 oldest IBI values
-      }
-
-      rate[9] = IBI;                          // add the latest IBI to the rate array
-      runningTotal += rate[9];                // add the latest IBI to runningTotal
-      runningTotal /= 10;                     // average the last 10 IBI values 
-      BPM = 60000/runningTotal;               // how many beats can fit into a minute? that's BPM!
-      QS = true;                              // set Quantified Self flag 
-      // QS FLAG IS NOT CLEARED INSIDE THIS ISR
-    }                       
+    }
   }
 
   if (Signal < thresh && Pulse == true){   // when the values are going down, the beat is over
@@ -77,20 +116,28 @@ void pulseISR(void) {
     P = thresh;                            // reset these for next time
     T = thresh;
   }
+  if (Signal2 < thresh2 && Pulse == true){   // when the values are going down, the beat is over
+    digitalWrite(blinkPin,LOW);            // turn off pin 13 LED
+    Pulse = false;                         // reset the Pulse flag so we can do it again
+    amp2 = P2 - T2;                           // get amplitude of the pulse wave
+    thresh2 = amp2/2 + T;                    // set thresh at 50% of the amplitude
+    P2 = thresh2;                            // reset these for next time
+    T2 = thresh2;
+  }
 
   if (N > 2500){                           // if 2.5 seconds go by without a beat
     thresh = 512;                          // set thresh default
     P = 512;                               // set P default
     T = 512;                               // set T default
-    lastBeatTime = sampleCounter;          // bring the lastBeatTime up to date        
+    thresh2 = 512;                          // set thresh default
+    P2 = 512;                               // set P default
+    T2 = 512;
+    lastBeatTime = sampleCounter;          // bring the lastBeatTime up to date
     firstBeat = true;                      // set these to avoid noise
     secondBeat = false;                    // when we get the heartbeat back
+    firstBeat2 = true;                      // set these to avoid noise
+    secondBeat2 = false;                    // when we get the heartbeat back
   }
 
     interrupts();
 }// end isr
-
-
-
-
-
--- firmware/PulseSensor_Spark.h
+++ firmware/PulseSensor_Spark.h
@@ -5,19 +5,29 @@
 #include "SparkIntervalTimer.h"
 
 volatile int rate[10];                    // array to hold last ten IBI values
+volatile int rate2[10];                    // array to hold last ten IBI values
 volatile unsigned long sampleCounter = 0;          // used to determine pulse timing
 volatile unsigned long lastBeatTime = 0;           // used to find IBI
 volatile int P =2048;                      // used to find peak in pulse wave, seeded
+volatile int P2 =2048;                      // used to find peak in pulse wave, seeded
 volatile int T = 2048;                     // used to find trough in pulse wave, seeded
+volatile int T2 = 2048;                     // used to find trough in pulse wave, seeded
 volatile int thresh = 2048;                // used to find instant moment of heart beat, seeded
+volatile int thresh2 = 2048;                // used to find instant moment of heart beat, seeded
 volatile int amp = 410;                   // used to hold amplitude of pulse waveform, seeded
+volatile int amp2 = 410;                   // used to hold amplitude of pulse waveform, seeded
 volatile boolean firstBeat = true;        // used to seed rate array so we startup with reasonable BPM
 volatile boolean secondBeat = false;      // used to seed rate array so we startup with reasonable BPM
 
+volatile boolean firstBeat2 = true;        // used to seed rate array so we startup with reasonable BPM
+volatile boolean secondBeat2 = false;      // used to seed rate array so we startup with reasonable BPM
+
 // these variables are volatile because they are used during the interrupt service routine!
 volatile int BPM;                   // used to hold the pulse rate
+volatile int BPM2;                   // used to hold the pulse rate
 volatile int Signal;                // holds the incoming raw data
-volatile int IBI = 600;             // holds the time between beats, must be seeded! 
+volatile int Signal2;                // holds the incoming raw data
+volatile int IBI = 600;             // holds the time between beats, must be seeded!
 volatile boolean Pulse = false;     // true when pulse wave is high, false when it's low
 volatile boolean QS = false;        // becomes true when Arduoino finds a beat.
 
@@ -26,6 +36,7 @@ volatile boolean QS = false;        // becomes true when Arduoino finds a beat.
 // So use A2 (though A0 & A1 ADC should still work) for pulse input, D7 (onboard LED) for blink LED and D6 for fancy LED
 //  VARIABLES
 int pulsePin = A2;				// Pulse Sensor purple wire connected to analog pin 0
+int pulsePin2 = A3;				// Pulse Sensor purple wire connected to analog pin 0
 int blinkPin = D7;				// pin to blink led at each beat
 int fadePin = D6;				// pin to do fancy classy fading blink at each beat
 int fadeRate = 0;				// used to fade LED on with PWM on fadePin
@@ -38,7 +49,3 @@ void pulseISR(void);
 IntervalTimer pulseTimer;
 
 #endif
-
-
-
-
